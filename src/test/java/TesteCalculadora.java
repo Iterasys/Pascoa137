2b726407886f2422ae005b040e221604132ba077
Iterasys <40407006+Iterasys@users.noreply.github.com>
2023-03-06 19:42:41 -0300

    Aula 15 - Teste de Unidade lendo arquivo csv

--- src/test/java/TesteCalculadora.java
+++ src/test/java/TesteCalculadora.java
@@ -1,6 +1,9 @@
 // Bibliotecas
 import br.com.iterasys.Calculadora;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.sql.SQLOutput;
 
@@ -27,6 +30,39 @@ public class TesteCalculadora { // inicio da classe
 
         // Valida
         assertEquals(resultadoEsperado, resultadoAtual);
+    } // final do teste do somar
+
+    // Este é um teste de unidade data driven - direcionado por dados
+    @ParameterizedTest
+    @CsvSource(value = {
+            "7, 5, 12.0",
+            "56, 44, 100.0",
+            "10, 0, 10.0",
+            "15, -5, 10.0",
+            "-8, -6, -14.0"
+    }, delimiter = ',')
+    public void testeSomarDoisNumerosLendoLista(String txtNum1, String txtNum2, String resultadoEsperado){ // inicio do teste do somar
+        // Configura
+        // Os dados de entrada e o resultado esperado vem da lista
+
+        // Executa
+        double resultadoAtual = Calculadora.somarDoisNumeros(Integer.valueOf(txtNum1), Integer.valueOf(txtNum2));
+
+        // Valida
+        assertEquals(Double.valueOf(resultadoEsperado), resultadoAtual);
+    } // final do teste do somar
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "csv/massaSomar.csv", numLinesToSkip = 1, delimiter = ',')
+    public void testeSomarDoisNumerosLendoArquivo(String txtNum1, String txtNum2, String resultadoEsperado){ // inicio do teste do somar
+        // Configura
+        // Os dados de entrada e o resultado esperado vem da lista
+
+        // Executa
+        double resultadoAtual = Calculadora.somarDoisNumeros(Integer.valueOf(txtNum1), Integer.valueOf(txtNum2));
+
+        // Valida
+        assertEquals(Double.valueOf(resultadoEsperado), resultadoAtual);
     } // final do teste do somar
 
     @Test
