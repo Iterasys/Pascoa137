7a7fe8f2de5836eae07f2e489164b4fe062c45e1
Iterasys <40407006+Iterasys@users.noreply.github.com>
2023-03-13 20:27:05 -0300

    Aula 17 - Teste de API - Post

--- src/test/java/apiTest/TesteUser.java
+++ src/test/java/apiTest/TesteUser.java
@@ -6,15 +6,47 @@ package apiTest;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+
 // Classe
 public class TesteUser {    // inicio da classe
     // Atributos
 
 
     // Funções e Métodos
-    @Test
-    public void testarIncluirUser(){
+    // Funções de Apoio
+    public static String lerArquivoJson(String arquivoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(arquivoJson)));
+    }
 
+    // Funções de Testes
+    @Test
+    public void testarIncluirUser() throws IOException {
+        // carregar os dados do nosso json
+        String jsonBody = lerArquivoJson("src/test/resources/json/user1.json");
+
+        String userId = "1371739181";
+
+        // realizar o teste
+        given()                                         // Dado que
+                .contentType("application/json")      // o tipo do conteúdo
+                .log().all()                            // mostre tudo
+                .body(jsonBody)                         // corpo da requisição
+        .when()                                         // Quando
+                .post("https://petstore.swagger.io/v2/user") // Endpoint / Onde
+        .then()                                         // Então
+                .log().all()                            // mostre tudo na volta
+                .statusCode(200)                      // comunic. ida e volta ok
+                .body("code", is(200))          // tag code é 200
+                .body("type", is("unknown"))    // tag type é "unknown"
+                .body("message", is(userId))         // message é o userId
+        ;
     }
 
 
