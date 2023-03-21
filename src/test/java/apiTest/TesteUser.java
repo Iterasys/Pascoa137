ea34b1f334428ea5364df9adc221580e422ad39b
Iterasys <40407006+Iterasys@users.noreply.github.com>
2023-03-20 21:51:53 -0300

    Aula 19 - Json Dinamico a partir de CSV - #bug

--- src/test/java/apiTest/TesteUser.java
+++ src/test/java/apiTest/TesteUser.java
@@ -8,6 +8,8 @@ package apiTest;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -139,4 +141,44 @@ public class TesteUser {    // inicio da classe
         String token = resp.jsonPath().getString("message").substring(23);
         System.out.println("Conteudo do Token: " + token);
     } // fim do login
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "csv/massaUser.csv", numLinesToSkip = 1, delimiter = ',')
+    public void testarIncluirUserCSV(
+            String id,
+            String username,
+            String firstName,
+            String lastName,
+            String email,
+            String password,
+            String phone,
+            String userStatus)
+    { // inicio Incluir CSV
+        // carregar os dados do nosso json
+        StringBuilder jsonBody = new StringBuilder("{");
+        jsonBody.append("'id': " + id + ",");
+        jsonBody.append("'username': " + username + ",");
+        jsonBody.append("'firstName': " + firstName + ",");
+        jsonBody.append("'lastName': " + lastName + ",");
+        jsonBody.append("'email': " + email + ",");
+        jsonBody.append("'password': " + password+ ",");
+        jsonBody.append("'phone': " + phone + ",");
+        jsonBody.append("'userStatus': " + userStatus);
+        jsonBody.append("}");
+
+        // realizar o teste
+        given()                                         // Dado que
+                .contentType(ct)                        // o tipo do conteúdo
+                .log().all()                            // mostre tudo
+                .body(jsonBody)                         // corpo da requisição
+        .when()                                         // Quando
+                .post(uriUser) // Endpoint / Onde
+        .then()                                         // Então
+                .log().all()                            // mostre tudo na volta
+                .statusCode(200)                      // comunic. ida e volta ok
+                .body("code", is(200))          // tag code é 200
+                .body("type", is("unknown"))    // tag type é "unknown"
+                .body("message", is(id))         // message é o userId
+        ;
+    } // fim incluir CSV
 } // fim da classe
