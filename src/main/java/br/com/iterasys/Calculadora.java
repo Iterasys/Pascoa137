99ccb6762c58af3321834d7a02a51fa67e94af4d
rafafcorreia <rafa.f.correia@gmail.com>
2023-03-27 20:50:02 -0300

    Novas funções para a calculadora

--- src/main/java/br/com/iterasys/Calculadora.java
+++ src/main/java/br/com/iterasys/Calculadora.java
@@ -11,7 +11,7 @@ public class Calculadora { // inicio da classe
     } // final da função somar
 
     public static double subtrairDoisNumeros(double num1, double num2){ // inicio da função subtrair
-         return num1 - num2;
+        return num1 - num2;
     } // final da função subtrair
 
     public static double multiplicarDoisNumeros(double num1, double num2){ // inicio da função multiplicar
@@ -44,9 +44,43 @@ public class Calculadora { // inicio da classe
 
     } // final da função dividir
 
+    public static double areaQuadrado(double aresta) {
+        return aresta * aresta;
+    }
+    public static double volumeCubo(double aresta) {
+        return Math.pow(aresta, 3);
+    }
+
+    public static double areaRetangulo(double base, double altura) {
+        return base * altura;
+    }
+
+    public static double volumeParalelepipedo(double base, double altura, double comprimento) {
+        return base * altura * comprimento;
+    }
+
+    public static double areaTriangulo(double base, double altura) {
+        return (base * altura) / 2;
+    }
+
+    public static double volumePiramideTriangular(double base, double altura, double comprimento) {
+        return (((base * altura) / 2) * comprimento) / 3;
+    }
+
+    public static double volumePiramideQuadrada(double base, double comprimento) {
+        return (Math.pow(base, 2) * comprimento) / 3;
+    }
+
+    public static double volumePiramideRetangular(double base, double altura, double comprimento) {
+        return (base * altura * comprimento) / 3;
+    }
+
+    public static double areaCirculo(double raio) {
+        return Math.pow(raio, 2) * 3.14;
+    }
+
+    public static double volumeCilindro(double raio, double altura) {
+        return Math.pow(raio, 2) * 3.14 * altura;
+    }
+
 } // final da classe
-
-
-
-
-
