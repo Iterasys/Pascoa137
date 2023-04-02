0379c7d140beae4f9117a656644313c71ef2dea0
Fernando teles <fernandoteles78@gmail.com>
2023-04-02 12:32:09 -0300

    Anotações

--- src/test/java/unitTest/TestVolumeCubo.java
+++ src/test/java/unitTest/TestVolumeCubo.java
@@ -9,14 +9,15 @@ public class TestVolumeCubo {
     @Test
     public void testCalcularVolumeCubo() {
         // Define a aresta do cubo
+        // Configura
         double comprimentoAresta = 5.0;
-        int potecia = 3;
 
         // Co cálculo do volume do cubo pode ser feito multiplicando o comprimento da aresta
         // por si mesma três vezes, ou seja, volume = comprimentoAresta * comprimentoAresta * comprimentoAresta.
         // Mas para simplificar o código e evitar repetições, é comum usar o método pow para calcular a potência
         // do número.
-        double volumeCalculado = Math.pow(comprimentoAresta, potecia);
+        //Executa
+        double volumeCalculado = Math.pow(comprimentoAresta, 3);
 
         // Define o resultado esperado
         double resultadoEsperado = 125.0;
@@ -24,13 +25,10 @@ public class TestVolumeCubo {
         // Este código está usando o método "assertEquals" para verificar se o volume calculado de um
         // cubo está correto.
         //0.0001: a margem de erro permitida para a comparação dos valores esperado e calculado.
-        assertEquals("O volume calculado do cubo deve ser " + resultadoEsperado + " unidades cúbicas",
-                resultadoEsperado, volumeCalculado, 0.0001);
+        //Valida
+        assertEquals(resultadoEsperado, volumeCalculado, 0.0001);
 
     }
-
-
-
 
 }
 
